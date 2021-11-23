384636510bcec97f5f411366362700ac1fe19be3
HipsterSantos <santoscampos269@gmail.com>
2021-11-23 02:36:45 +0100

    tweaking authenticatio

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ router.post('/', async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send('Invalid email or password.');
+  if (!user) return res.status(400).send('This email or password is Invalid');
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send('Invalid email or password.');
